7527d47c3caa3f99b46e21aeaffd4e1bdf17febe
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-07-28 14:01:07 +0600

    typeof variable

---
+++ type.js
@@ -0,0 +1,14 @@
+// String
+var name = 'sadar uddin';
+var address = "Andar killa bandorban with some katabon";
+var country = 'Bangladesh';
+
+// boolean
+var isRich = true;
+
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
+console.log(typeof country);
+var price = 555;
+console.log(typeof price);
