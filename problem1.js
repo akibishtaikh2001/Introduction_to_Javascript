0e7b95b67678e6f75d0ddf22521f1f13e18414dd
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-07-29 13:02:15 +0600

    Practice Problem 1

---
+++ problem1.js
@@ -0,0 +1,6 @@
+var iHaveMoney = 1000;
+var orangePrice = 400;
+var applePrice = 300;
+var totalShoping = orangePrice + applePrice;
+var shopkeeperWillReturn = iHaveMoney - totalShoping;
+console.log(shopkeeperWillReturn);
