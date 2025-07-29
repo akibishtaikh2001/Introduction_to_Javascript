aafe910c19021f5c932273b756da085a6cbf299d
akibishtaikh2001 <akibishtiakh2220@gmail.com>
2025-07-29 13:09:21 +0600

    Practice Problem 2

---
+++ problem2.js
@@ -0,0 +1,9 @@
+var Mathematics = 75.25;
+var Biology = 65;
+var Chemistry = 80;
+var Physics = 35.45;
+var Bangla = 99.50;
+var totalNumber = Mathematics + Biology + Chemistry + Physics + Bangla;
+var Average = totalNumber/ 5;
+
+console.log(Average.toFixed(2))
